5d6db2577d520ad48ebd7a038703c3d799382a32
borysinho <borysquiroga@gmail.com>
2024-06-24 04:58:00 -0400

    adding dist empty folder

--- dist/src/objects/twilio.object.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const twilio_1 = require("twilio");
-// console.log(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
-exports.default = new twilio_1.Twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN, {
-    autoRetry: true,
-    lazyLoading: true,
-    maxRetries: 2,
-});
