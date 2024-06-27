49d7be3cd52314a71e030a61bc74e26998f45dab
borysinho <borysquiroga@gmail.com>
2024-06-27 19:51:12 -0400

    products_working2

--- src/controllers/twilio.controller.ts
+++ src/controllers/twilio.controller.ts
@@ -61,94 +61,11 @@ export const testLC = catchedAsync(async (req: Request, res: Response) => {
     }
   );
 
-  // // console.log({ IAAnser: result.output });
-
-  // const message = await sendWhatsappMessage(whatsappNumber, result.output);
+  const message = await sendWhatsappMessage(whatsappNumber, result.output);
 
   console.log({ output: result.output });
 
   response(res, 200, result.output as string);
-
-  // const result = await ofrecerProductosYServicios(
-  //   texto,
-  //   whatsappNumber,
-  //   profileName
-  // );
-  // const result = await buildAgent(texto);
-
-  // response(res, 200, message);
-
-  // const chats = await srvObtenerChatDesdeWhatsapp(whatsappNumber);
-
-  // const result = await test14(texto, whatsappNumber, profileName);
-
-  // response(res, 200, result);
-
-  // const vectorStore = await pgVectorStore();
-
-  // const filter = (vectorStore.filter = {
-  //   profileName,
-  // });
-
-  // console.log({ filter });
-
-  // return response(res, 200, "OK");
-
-  // const result = await chatearConMemoria(texto, profileName, whatsappNumber);
-  // const result = await test2(texto, whatsappNumber);
-  // const chatHistory = await test4(texto, whatsappNumber);
-  // const result = await chatHistory.invoke(
-  //   {
-  //     input: texto,
-  //   },
-  //   { configurable: { sessionId: whatsappNumber } }
-  // );
-
-  // console.log({ result });
-
-  // return response(res, 200, result);
-
-  // console.log({ cliente });
-
-  //==============================================
-  // const chatHistory = await srvChainConHistoria2();
-  // const chatContextualizado = await chatHistory.invoke(
-  //   {
-  //     input: texto,
-  //   },
-  //   { configurable: { sessionId: whatsappNumber } }
-  // );
-
-  // console.log({ chatContextualizado });
-  // response(res, 200, chatContextualizado);
-  //==============================================
-
-  // const similitud = await srvBuscarSimilaridad(chatContextualizado, 5);
-  // console.log({ chatContextualizado, similitud });
-
-  // const pageContent = similitud.map((s) => s.pageContent).join("\n");
-
-  // const document = new Document({
-  //   pageContent,
-  // });
-
-  // console.log({ document });
-
-  // console.log({ chatContextualizado });
-  // const retrievalChain = await test13();
-
-  // const result = await retrievalChain.invoke({
-  //   input: chatContextualizado,
-  // });
-  // const result = await retrievalChain.invoke({
-  //   input: chatContextualizado,
-  //   context: similitud,
-  // });
-
-  // console.log({ answer: retrievalChain });
-
-  // // console.log({ result });
-  // response(res, 200, result.answer);
 });
 
 export const indexDataSource = catchedAsync(
